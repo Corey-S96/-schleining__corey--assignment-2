0edf5ed8eb325022a005ee02ddedfc7d3c2984da
Corey-S96 <schleiningcorey@gmail.com>
2021-06-02 14:47:44 -0500

    Scrapped the template given in starter files, added working To Do List, added Done and Delete buttons, page focuses on input field on page load, gradient style added for background.

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+    // icons for list items pulled from lineicons folder //
+    $('#input').change(function(){
+        let input = $(this).val();
+        $('ul').append('<li>' + input + ' <i class="lni lni-checkmark-circle"></i><i class="lni lni-cross-circle"></i></li>');
+        $(this).val();
+    });
+
+    $('ul').on('click', '.lni-cross-circle', function(){
+        $(this).parent('li').fadeOut(200);
+    });
+
+    $('ul').on('click', '.lni-checkmark-circle', function(){
+        $(this).parent('li').toggleClass('checked');
+    });
+
+});
