4603ece80b30db1c8a63b5e73ecafd29493537a4
Corey-S96 <schleiningcorey@gmail.com>
2021-06-11 19:44:28 -0500

    Adding final touches before submission

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ $(document).ready(function(){
     
     // icons for list items pulled from lineicons folder //
     $('#input').change(function(){
+
         let input = $(this).val();
         $('ul').append('<li>' + input + ' <i class="lni lni-checkmark-circle"></i><i class="lni lni-cross-circle"></i></li>');
         $(this).val();
     });
 
+    // Delete //
     $('ul').on('click', '.lni-cross-circle', function(){
+
         $(this).parent('li').fadeOut(200);
     });
 
+    // Mark as "Done" //
     $('ul').on('click', '.lni-checkmark-circle', function(){
+        
         $(this).parent('li').toggleClass('checked');
     });
 
